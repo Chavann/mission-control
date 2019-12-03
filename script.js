86b345f100c831ebfc9135b9a63aed7fb12b0a36
Chavann <56319762+Chavann@users.noreply.github.com>
2019-12-03 10:17:38 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+script.js
+
+<script language="javascript">
+
+document.getElementsByClassName('center.buttonwrong') function incorrectfunction() {
+  alert('Incorrect!');
+}
+
+document.getElementsByClassName('buttonright') function(){
+  alert("Correct!");
+}
+
+function incorrectfunction(){
+  alert('Incorrect!');
+}
+>>>>>>> 3541598850fd0966f903b780d4c6648ae6f1e3b3
