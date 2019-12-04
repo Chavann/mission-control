edb578e8d65dd6be8a0b900ec1ecc770ad1e0c16
Chavann <Chavann@users.noreply.github.com>
2019-12-03 17:25:13 -0700

    stuffff

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 script.js
 
 <script language="javascript">
-  document.getElementById('quiz').innerHTML = "Fun Facts Quiz Time!";
-  function (quiz) {}
 
-  var questions  [
-  {
-    prompt: "What is the hottest Planet?\n (a)Mercury\n (b)Mars\n (c)Juptiter\n (d)Saturn",
-    answer: "c"}
-  ]
-  var score = 0;
-  for(var i=0; i < questions.length; i++){
-    var response = window.prompt (questions[i].prompt);
-    if(response == questions[i].answer){
-      score++;
-      alert("Right!");
-    } else {
-      alert("Wrong!");
-    }
-  }
+document.getElementsByClassName('center.buttonwrong') function incorrectfunction() {
+  alert('Incorrect!');
+}
 
-function myfucntion(){
-  alert("Wrong :(");
+document.getElementsByClassName('buttonright') function(){
+  alert("Correct!");
+}
+
+function incorrectfunction(){
+  alert('Incorrect!');
 }
